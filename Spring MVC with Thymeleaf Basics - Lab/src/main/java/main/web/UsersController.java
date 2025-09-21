a2093115d7fa7e37410b2681ded1142bef412532
Cavani99 <kristiian.donchev@abv.bg>
2025-09-21 22:43:18 +0300

    Made Task 2 for Spring MVC with Thymeleaf Basics - Lab

--- Spring MVC with Thymeleaf Basics - Lab/src/main/java/main/web/UsersController.java
+++ Spring MVC with Thymeleaf Basics - Lab/src/main/java/main/web/UsersController.java
@@ -4,6 +4,7 @@ import main.model.User;
 import main.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +30,18 @@ public class UsersController {
 
         modelAndView.addObject("users", users);
         modelAndView.setViewName("users.html");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView userDetails(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        User user = userService.getUserById(id);
+
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("user-details.html");
 
         return modelAndView;
     }
